1340fb7dd055d871acf7950b6570bb8bdfecee3f
alexeyvyduykin <alexeyvyduykin@protonmail.com>
2021-03-31 08:51:49 +0300

    Add GroundStation render model

--- src/Globe3DLight/ViewModels/Scene/RenderModels/GroundStationRenderModel.cs
+++ src/Globe3DLight/ViewModels/Scene/RenderModels/GroundStationRenderModel.cs
@@ -1,13 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Immutable;
 using Globe3DLight.Models.Geometry;
+using Globe3DLight.ViewModels.Geometry.Models;
 
 namespace Globe3DLight.ViewModels.Scene
 {
     public class GroundStationRenderModel : BaseRenderModel
     {
-        public IAMesh Mesh { get; set; }
-
+        //public IAMesh Mesh { get; set; }
+        public IMesh Mesh { get; set; }
         public double Scale { get; set; }
     }
 }
